afe7360f672e5c8c795c36dc9bd49dc578919478
Jesse Levandovski <961151@lojasrenner.com.br>
2023-03-08 11:04:52 -0300

    commit the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  function getNames() {
+    document.querySelector("span").innerHTML = `${name} ${lastName}`;
+  }
+
+  const handleClick = () => {
+    if (!name || !lastName) alert("Fill in name and lastname");
+
+    alert("Registration done successfully");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <span></span>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          width: "500px",
+          height: "500px",
+          background: "#cccddd",
+        }}
+      >
+        <p>Learning tests to react</p>
+
+        <input
+          style={{ marginBottom: "15px" }}
+          aria-label="name-input"
+          placeholder="Name"
+          type="text"
+          value={name}
+          onChange={handleChangeName}
+        />
+
+        <input
+          style={{ marginBottom: "15px" }}
+          aria-label="lastName-input"
+          placeholder="Last Name"
+          type="text"
+          value={lastName}
+          onChange={handleChangeLastName}
+        />
+
+        <button aria-label="register-button" onClick={handleClick}>
+          Register
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+const setup = () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learning tests to react/i);
+  const inputName = screen.getByLabelText("name-input");
+  const inputLastName = screen.getByLabelText("lastName-input");
+  const buttonRegister = screen.getByLabelText("register-button");
+
+  return {
+    linkElement,
+    inputName,
+    inputLastName,
+    buttonRegister,
+  };
+};
+
+test("renders learn react link", () => {
+  const { linkElement } = setup();
+
   expect(linkElement).toBeInTheDocument();
+});
+
+test("testing inputs", () => {
+  const { inputName, inputLastName, buttonRegister, spanFullName } = setup();
+
+  fireEvent.change(inputName, { target: { value: "João" } });
+  expect(inputName.value).toBe("João");
+
+  fireEvent.change(inputLastName, { target: { value: "da Silva" } });
+  expect(inputLastName.value).toBe("da Silva");
+
+  userEvent.click(buttonRegister);
+
+  expect(
+    screen.getByText("Registration done successfully")
+  ).toBeInTheDocument();
 });
